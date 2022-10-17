7f5bf61162a41680ad051a77cb5dad956547c8a3
Jared Glickfield <ubuntu@ubuntu2004.linuxvmimages.local>
2022-10-17 00:06:36 -0400

    Jquery and Gauges

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
-google.charts.load('current', {'packages':['gauge']});
-google.charts.setOnLoadCallback(drawChart);
+$(function(){
+  var colWidth = $(".col-lg-3").width();
+  $(".reactor-temp").tempGauge({
+    showLabel: true, 
+    showScale: true,
+    maxTemp: 100,
+    minTemp: 0,
+    width: colWidth,
+    currentTemp: 80,
+    labelSize: 12
+  });
+  
+  $(".coolent-temp").tempGauge({
+    showLabel: true, 
+    showScale: true,
+    maxTemp: 100,
+    minTemp: 0,
+    width: colWidth,
+    currentTemp: 0,
+    labelSize: 12
+  });
 
-function drawChart() {
-
-  var data = google.visualization.arrayToDataTable([
-    ['Label', 'Value'],
-    ['Reactor Core', 0],
-    ['Coolant', 0],
-    ['Power Put', 0]
-  ]);
-
-  var options = {
-    width: 720, height: 240,
-    redFrom: 90, redTo: 100,
-    yellowFrom:75, yellowTo: 90,
-    greenFrom:0, greenTo:75,
-    minorTicks: 5
-  };
-
-  var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-
-  chart.draw(data, options);
-
-  setInterval(function() {
-    data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
-    chart.draw(data, options);
-  }, 13000);
-  setInterval(function() {
-    data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
-    chart.draw(data, options);
-  }, 5000);
-  setInterval(function() {
-    data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
-    chart.draw(data, options);
-  }, 26000);
-}
+  $(".power-out").gauge(10000, {
+    unit: "Volts",
+    min: 0,
+    max: 100000,
+    color: "red"
+  });
+});
