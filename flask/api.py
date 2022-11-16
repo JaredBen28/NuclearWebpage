39f0c8a0c87fbbf17b0fcb99e569f0a1c17120ae
Jared Glickfield <ubuntu@ubuntu2004.linuxvmimages.local>
2022-11-16 11:56:02 -0500

    live sim to webpage

--- flask/api.py
+++ flask/api.py
@@ -1,19 +1,30 @@
 from random import randint, random
 import resource
+import csv
 from flask import Flask, jsonify
 from flask_cors import cross_origin
 
+lock = 0
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def get_example():
+    file = open("/var/www/NuclearWebpage/flask/sample.csv", "r")
+    data = list(csv.reader(file, delimiter=","))
+    file.close()
+
     response = {
-        'power': randint(0,100), 
-        'coreTemp': randint(0,100), 
-        'coolentTemp': randint(0,100)
+        'power': 0, 
+        'coreTemp': data[0][7], 
+        'coolentTemp': -1
     }
     return jsonify(response)
+
+@app.route("/test", methods=['GET'])
+@cross_origin()
+def get_test():
+    return lock
 
 if __name__ == '__main__':
     app.run()
